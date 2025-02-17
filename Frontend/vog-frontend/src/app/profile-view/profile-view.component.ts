bdf49539d2493f61970ea468a4eaf463d9dfa0d2
Douglas <doggek@hotmail.se>
2025-02-17 12:51:21 +0100

    Starting profile page, working on a concept for profile pictures

--- Frontend/vog-frontend/src/app/profile-view/profile-view.component.ts
+++ Frontend/vog-frontend/src/app/profile-view/profile-view.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { BacklogService } from '../../services/backlog.service';
+import { GetBacklogResponse } from '../../vog-api';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-profile-view',
@@ -7,4 +10,36 @@ import { Component } from '@angular/core';
   templateUrl: './profile-view.component.html',
   styleUrl: './profile-view.component.scss',
 })
-export class ProfileViewComponent {}
+export class ProfileViewComponent {
+  userId: number | null = null;
+  userName: string = '';
+  backlogAmount: number | null = null;
+  backlogItems: GetBacklogResponse[] = [];
+
+  constructor(
+    private backlogService: BacklogService,
+    private userService: UserService
+  ) {}
+
+  ngOnInit(): void {
+    this.userId = this.userService.userId;
+    this.userName = this.userService.userName;
+    this.loadBacklog();
+  }
+
+  loadBacklog(): void {
+    this.backlogService.getUserBacklog(this.userId!).subscribe(
+      data => {
+        this.backlogItems = data;
+        this.countBacklogGames();
+      },
+      error => {
+        console.error('Error fetching backlog:', error);
+      }
+    );
+  }
+
+  countBacklogGames(): void {
+    this.backlogAmount = this.backlogItems.length;
+  }
+}
